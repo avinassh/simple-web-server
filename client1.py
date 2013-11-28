e6785d2e95340f2223e66be2c3d9178dba706088
avinassh <avinashsajjan@gmail.com>
2013-11-28 14:25:02 +0530

    changed port number in client.py

--- client1.py
+++ client1.py
@@ -1,5 +1,5 @@
 import urllib2
-req = urllib2.Request(url='http://localhost:8000', data='This is POST data')#,data='This data is passed to stdin of the CGI')
-urllib2.urlopen(req)
+req = urllib2.Request(url='http://localhost:8080')#, data='This is POST data')#,data='This data is passed to stdin of the CGI')
+print urllib2.urlopen(req).read()
 #print f.read()
 #Got Data: "This data is passed to stdin of the CGI"

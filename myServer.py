0cd411cee6db155e03cb1b0948f1dafc037d4dbb
avinassh <avinashsajjan@gmail.com>
2013-12-03 14:49:14 +0530

    added construct_message method, more readability

--- myServer.py
+++ myServer.py
@@ -2,6 +2,8 @@ import time
 import BaseHTTPServer
 import urlparse
 import urllib 
+import SocketServer
+import threading
 
 HOST_NAME = 'localhost'
 PORT_NUMBER = 8089
@@ -16,59 +18,60 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     def do_GET(self):
         """ Respond to a GET request. """
-        
-        #add a class to return stuff
-        vm_number = 20
-        vm_name = 'Cognitive Science'
 
-        # self.send_response(200)
-        # self.send_header("Content-type", "text/html")
-        # self.end_headers()
         self.do_HEAD()
 
         if self.path == '/vmnum/':
-            self.wfile.write("<html><head><title>One VM Per Lab</title></head>")
-            self.wfile.write("<body><p>Success!</p>")
-            self.wfile.write("<p>Currently there are %s virtual machines are running" % vm_number)
-            self.wfile.write("<p>You accessed path: %s</p>" % self.path)
-            self.wfile.write("</body></html>")
+            response = self.construct_message('Currently there are no VMs running')
+            self.wfile.write(response)
         elif self.path == '/vmname/':
-            self.wfile.write("<html><head><title>One VM Per Lab</title></head>")
-            self.wfile.write("<body><p>Success!</p>")
-            self.wfile.write("<p>Current VM name is %s" % vm_name)
-            self.wfile.write("<p>You accessed path: %s</p>" % self.path)
-            self.wfile.write("</body></html>")
+            respose = self.construct_message('Your VM is yet to be named')
+            self.wfile.write(respose)
         else:
-            self.wfile.write("<html><head><title>One VM Per Lab</title></head>")
-            self.wfile.write("<body><p>This is a test.</p>")
-            self.wfile.write("<p>You accessed path: %s</p>" % self.path)
-            self.wfile.write("</body></html>") 
-        # reply back in JSON
-        # 
+            response = self.construct_message()
+            self.wfile.write(response)
         return
             
 
     def do_POST(self):
         """ Respond to a POST request """
         length = int(self.headers['Content-Length'])
+
         #http://stackoverflow.com/a/12731208/1382297
         post_data = urlparse.parse_qs(self.rfile.read(length).decode('utf-8'))
+        thread_number = threading.currentThread().getName()
         lab_name = str(post_data.get('lab-name')[0])
         lab_author = str(post_data.get('lab-author')[0])
+        
         #print "%s/?%s" % (self.path, urllib.urlencode(post_data))
         #print "%s/?%s" % (self.path, urllib.urlencode({k:v[0] for k,v in post_data.iteritems()}))
-        self.wfile.write("<html><head><title>One VM Per Lab</title></head>")
-        self.wfile.write("<body><p>This is a test.</p>")
-        self.wfile.write("<p>You successfully created a VM named: %s at the location: %s</p>" % (lab_name, lab_author))
-        self.wfile.write("</body></html>")        
-        return    
 
+        message = "<p>You successfully created a VM named: " + lab_name + " at the location: " + lab_author + "</p><p>\
+        \nYou have been served from the thread: " + thread_number
+        respose = self.construct_message(message)
+
+        self.wfile.write(respose)
+        return
+
+    def construct_message(self, message=''):
+        message_part1 = "<html><head><title>One VM Per Lab</title></head><body><p>Success!</p>"
+        message_part2 = "<p>You accessed path: " + self.path + "</p>"
+        message_part3 = "</body></html>"
+        return message_part1 + message_part2 + message + message_part3
+
+class ThreadedHTTPServer(SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer):
+    """Handle requests in a separate thread."""
+    pass
 
 if __name__ == '__main__':
-    server_class = BaseHTTPServer.HTTPServer
-    httpd = server_class((HOST_NAME, PORT_NUMBER), Handler)
-    print "Server Started - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    #httpd = server_class((HOST_NAME, PORT_NUMBER), Handler)
+    httpd = ThreadedHTTPServer((HOST_NAME, PORT_NUMBER), Handler)
     try:
+        print "Server Started - %s:%s with the thread :%s" % (HOST_NAME, PORT_NUMBER, server_thread.name)
+        # server_thread = threading.Thread(target=httpd.serve_forever)
+        # Exit the server thread when the main thread terminates
+        # server_thread.daemon = True
+        # server_thread.start()
         httpd.serve_forever()
     except KeyboardInterrupt:
         httpd.server_close()
